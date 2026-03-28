1e9fb4a17ad77e90075b5620e1ddee85d320bd53
andrealoda <andrea@casaloda.it>
2026-03-28 19:07:57 +0100

    snack 4 complete

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorsNames = posts.map(pickNames => pickNames.author)
+console.log(authorsNames);
