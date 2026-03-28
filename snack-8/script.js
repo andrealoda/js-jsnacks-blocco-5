862a3f9a4192cf92d4ca0538ea334b7726970fdc
andrealoda <andrea@casaloda.it>
2026-03-28 19:27:10 +0100

    snack 8 complete

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const result = students.find(stud => stud.name === 'Marco Lanci')
+console.log(result.class);
