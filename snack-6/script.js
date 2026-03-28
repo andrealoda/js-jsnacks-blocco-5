2f2698129ffb14e9f8354cc39bf3f2d9793048f5
andrealoda <andrea@casaloda.it>
2026-03-28 19:19:34 +0100

    snack 6 complete

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,10 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const moreThan15 = zucchine.filter(zucchino => zucchino.length >= 15)
+
+const lessThan15 = zucchine.filter(zucchino => zucchino.length < 15)
+
+console.log(moreThan15);
+console.log(lessThan15);
